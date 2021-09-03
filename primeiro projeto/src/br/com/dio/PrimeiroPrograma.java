1e022280417e86059d1b3bd8221f11c32553ef4c
FlavioNogueira <flavionogueira20@gmail.com>
2021-09-03 00:52:16 -0300

    Atualizando projeto

--- primeiro projeto/src/br/com/dio/PrimeiroPrograma.java
+++ primeiro projeto/src/br/com/dio/PrimeiroPrograma.java
@@ -4,10 +4,7 @@ public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-<<<<<<< HEAD
 		System.out.println("Adicionando mais uma linha");
-=======
->>>>>>> f17a5248f920261f173841f7952f704af498202b
 	}
 
 }
